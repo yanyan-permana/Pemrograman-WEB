93d1993fce21da388e60cd67d97e2587eeffedbe
yanyan-permana <deyanyan6@gmail.com>
2022-04-14 14:10:35 +0700

    Membuat proses simpan data mahasiswa

--- absensi-newlayout/input_mahasiswa.php
+++ absensi-newlayout/input_mahasiswa.php
@@ -1,22 +1,22 @@
-<?php 
-	include './layout/header.php';
-	$action_menu=$_GET['action_menu'];
+<?php
+include './layout/header.php';
+$action_menu = $_GET['action_menu'];
 ?>
 
 <!-- Navigation -->
 </div>
-	<div class="row wrapper border-bottom white-bg page-heading">
-		<div class="col-lg-10">
-			<h2>Input Mahasiswa</h2>
-			<ol class="breadcrumb">
-				<li class="breadcrumb-item">
-					<a href="index.php">Home</a>
-				</li>
-				<li class="breadcrumb-item">
-					<a>Data Master</a>
-				</li>
-			</ol>
-		</div>
+<div class="row wrapper border-bottom white-bg page-heading">
+	<div class="col-lg-10">
+		<h2>Input Mahasiswa</h2>
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item">
+				<a href="index.php">Home</a>
+			</li>
+			<li class="breadcrumb-item">
+				<a>Data Master</a>
+			</li>
+		</ol>
+	</div>
 </div>
 
 
@@ -29,40 +29,40 @@
 					<h5>Input Mahasiswa</h5>
 				</div>
 				<div class="ibox-content">
-					<form method="post" action="simpan_mahasiswa.php">
+					<form id="input_form" method="post" action="simpan_mahasiswa.php">
 						<div class="form-group  row"><label class="col-sm-2 col-form-label">NIM</label>
-							<div class="col-sm-10"><input type="text" class="form-control" name="nim"></div>
+							<div class="col-sm-10"><input id="nim" type="text" class="form-control" name="nim"></div>
 						</div>
 						<div class="form-group  row"><label class="col-sm-2 col-form-label">Nama</label>
-							<div class="col-sm-10"><input type="text" class="form-control" name="nama_mhs"></div>
+							<div class="col-sm-10"><input id="nama_mhs" type="text" class="form-control" name="nama_mhs"></div>
 						</div>
 						<div class="form-group row"><label class="col-sm-2 col-form-label">Jenis Kelamin</label>
 
-							<div class="col-sm-10"><select class="form-control m-b" name="jenis_kelamin">
-								<option value="L">Laki-Laki</option>
-								<option value="P">Perempuan</option>
-							</select>
+							<div class="col-sm-10"><select id="jenis_kelamin" class="form-control m-b" name="jenis_kelamin">
+									<option value="L">Laki-Laki</option>
+									<option value="P">Perempuan</option>
+								</select>
 							</div>
 						</div>
-						 <div class="form-group row" id="data_1"><label class="col-sm-2 col-form-label">Tanggal Lahir</label>
+						<div class="form-group row" id="data_1"><label class="col-sm-2 col-form-label">Tanggal Lahir</label>
 							<div class="col-sm-10 input-group date">
-								<span class="input-group-addon"><i class="fa fa-calendar"></i></span><input type="text" class="form-control" value="03/04/2014" name="tgl_lahir">
+								<span class="input-group-addon"><i class="fa fa-calendar"></i></span><input id="tgl_lahir" type="text" class="form-control" name="tgl_lahir">
 							</div>
 						</div>
 						<div class="form-group  row"><label class="col-sm-2 col-form-label">Alamat</label>
-							<div class="col-sm-10"><textarea type="text" class="form-control" name="alamat"></textarea></div>
+							<div class="col-sm-10"><textarea id="alamat" type="text" class="form-control" name="alamat"></textarea></div>
 						</div>
 						<div class="form-group  row"><label class="col-sm-2 col-form-label">No Telepon</label>
-							<div class="col-sm-10"><input type="text" class="form-control" name="no_telp"/></div>
+							<div class="col-sm-10"><input id="no_telp" type="text" class="form-control" name="no_telp" /></div>
 						</div>
 						<div class="form-group  row"><label class="col-sm-2 col-form-label">Email</label>
-							<div class="col-sm-10"><input type="text" class="form-control" name="email"></div>
+							<div class="col-sm-10"><input id="email" type="text" class="form-control" name="email"></div>
 						</div>
 						<div class="form-group row"><label class="col-sm-2 col-form-label">Semester</label>
-							<div class="col-sm-10"><input type="text" class="form-control" name="semester"></div>
+							<div class="col-sm-10"><input id="semester" type="text" class="form-control" name="semester"></div>
 						</div>
 						<div class="hr-line-dashed"></div>
-						
+
 						<div class="form-group row">
 							<div class="col-sm-4 col-sm-offset-2">
 								<button class="btn btn-white btn-sm" onclick="history.back()" type="button">Cancel</button>
@@ -75,25 +75,52 @@
 		</div>
 	</div>
 </div>
-	
+
 <!-- Jquery -->
 <script>
-	$(document).ready(function () {
-		$('.i-checks').iCheck({
-			checkboxClass: 'icheckbox_square-green',
-			radioClass: 'iradio_square-green',
-		});
-		
-		 $('#data_1 .input-group.date').datepicker({
+	$(document).ready(function() {
+
+		$('#data_1 .input-group.date').datepicker({
 			todayBtn: "linked",
 			keyboardNavigation: false,
 			forceParse: false,
 			calendarWeeks: true,
 			autoclose: true
+		});
+
+		$('#input_form').submit(function(e) {
+			e.preventDefault();
+			$.ajax({
+				type: "POST",
+				url: 'simpan_mahasiswa.php',
+				data: $(this).serialize(),
+				success: function(response) {
+					var res = JSON.parse(response);
+					if (res.status == 1) {
+						alert('data sudah disimpan');
+
+						// <!-- reset nilai element input -->
+						$('#nim').val("");
+						$('#nama_mhs').val("");
+						$('#jenis_kelamin').val(1);
+						$('#tgl_lahir').val("");
+						$('#alamat').val("");
+						$('#no_telp').val("");
+						$('#email').val("");
+						$('#semester').val("");
+
+						$('#nim').focus();
+					} else {
+						alert('data gagal disimpan');
+					}
+				},
+				error: function(response) {
+					alert('data gagal disimpan');
+				}
 			});
+		});
 	});
-	
 </script>
 
 
-<?php include './layout/footer.php';?>
+<?php include './layout/footer.php'; ?>
--- absensi-newlayout/simpan_mahasiswa.php
+++ absensi-newlayout/simpan_mahasiswa.php
@@ -1,42 +1,32 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<head>
-	<title>Proses Simpan Data...</title>
-</head>
-
-<body>
-	<?php
-	// var_dump($_POST);
-	// die;
-	include 'mysql_connect.php';
-
-	// menyimpan data kedalam variabel
-	$nim            = $_POST['nim'];
-	$nama_mhs       = $_POST['nama_mhs'];
-	$jenis_kelamin  = $_POST['jenis_kelamin'];
-	$tgl_lahir      = date("Y-m-d", strtotime($_POST['tgl_lahir']));
-	$alamat         = $_POST['alamat'];
-	$no_telp        = $_POST['no_telp'];
-	$email          = $_POST['email'];
-	$semester       = $_POST['semester'];
-
-	$insert_query = "insert into master_mahasiswa (nim, nama, jenis_kelamin, tanggal_lahir, alamat, no_tlp, email, semester) values ('$nim', '$nama_mhs','$jenis_kelamin','$tgl_lahir','$alamat','$no_telp','$email','$semester')";
-
-	// menjalankan perintah insert ke tabel
-	$result = mysqli_query($conn, $insert_query);
-	if (!$result) {
-		echo "Gagal simpan data!";
-	} else {
-		echo "Data sudah disimpan!";
+	try {
+		include 'mysql_connect.php';
+		 
+		// menyimpan data kedalam variabel
+		$nim            = $_POST['nim'];
+		$nama_mhs       = $_POST['nama_mhs'];
+		$jenis_kelamin  = $_POST['jenis_kelamin'];
+		$tgl_lahir      = date("Y-m-d", strtotime($_POST['tgl_lahir']));
+		$alamat         = $_POST['alamat'];
+		$no_telp        = $_POST['no_telp'];
+		$email          = $_POST['email'];
+		$semester       = $_POST['semester'];
+		
+		$insert_query = "insert into master_mahasiswa (nim, nama, jenis_kelamin, tanggal_lahir, alamat, no_tlp, email, semester) values ('$nim', '$nama_mhs','$jenis_kelamin','$tgl_lahir','$alamat','$no_telp','$email','$semester')";
+		
+		// menjalankan perintah insert ke tabel
+		$result = mysqli_query($conn, $insert_query);
+		if (!$result) {
+			echo json_encode(array("status"=>0));
+		} else {
+			echo json_encode(array("status"=>1));
+		}
+		 
+	} catch (Exception $ex) {
+		echo $ex->getMessage();
+	} finally {
+		// menutup koneksi
+		mysqli_close($conn);
 	}
-
-	// menutup koneksi
-	mysqli_close($conn);
-
-	?>
-	<br>
-	<a href="/pemrograman-web/absensi-newlayout/input_mahasiswa.php?action_menu=INSERT"> Go back </a>
-</body>
-
-</html>
+?>
